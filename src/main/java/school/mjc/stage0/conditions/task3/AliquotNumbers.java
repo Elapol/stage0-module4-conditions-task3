5946403eafc176abf545d1a04f4b21e16c1f4bd9
elyad <ella.dikovich@gmail.com>
2022-11-01 14:36:03 +0100

    elka11

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public static void main(String[] args) {
-        isFirstAliquot(11,15);
+        isFirstAliquot(5,12);
     }
     public static void isFirstAliquot(int first, int second) {
-        if (first == 5 || second == 6){
+        if (first >= 0 && first<second && first!= second) {
             System.out.println("Aliquot");
         } else {
             System.out.println("Not aliquot");
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,3 +1,5 @@
+package school.mjc.stage0.conditions.task3;
+
 public class IsEnglishSymbolDeterminer {
     public static void main(String[] args) {
         isEnglishSymbol('a');
